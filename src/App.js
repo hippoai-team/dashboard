5c6eb3c8809c7d538f208f8749954b80606d607c
ShaniGotSkills <89371012+ShaniGotSkills@users.noreply.github.com>
2023-09-16 23:12:57 +0500

    unguard 2

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Routes>
 
                 // <Route path="/dashboard" element={<PrivateRoute />} >
-                  <Route path='' element={<Dashboard />} />
+                  <Route path='/dashboard' element={<Dashboard />} />
                 // </Route>
 
                 <Route path="/books" element={<PrivateRoute />} >
