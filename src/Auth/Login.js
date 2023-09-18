6b6222b92bb13df8a38e3618d324a49ff7b25c82
ShaniGotSkills <89371012+ShaniGotSkills@users.noreply.github.com>
2023-09-18 17:19:46 +0500

    u5

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('/admin/login', {
+            const res = await axios.post('https://express-vercel-demo-six.vercel.app/admin/login', {
                 email,
                 password
             });
