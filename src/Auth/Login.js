a829474f6b5a48937a807c432570a9044d599d5f
ShaniGotSkills <89371012+ShaniGotSkills@users.noreply.github.com>
2023-09-18 10:35:00 +0500

    nroute 1

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/admin/login', {
+            const res = await axios.post('https://mern-bookstore.vercel.app/admin/login', {
                 email,
                 password
             });
