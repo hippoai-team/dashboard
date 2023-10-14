0a050f8175cd66a89b0f3b618f7e457d4e3e541e
OmriNach <omnachmani@gmail.com>
2023-10-14 10:30:33 -0400

    usage counts

--- src/components/BetaList.js
+++ src/components/BetaList.js
@@ -292,6 +292,7 @@ const BetaList = () => {
                             <th style={{ width: "10px" }}>Delete</th>
                             <th style={{ width: "70px" }}>Email</th>
                             <th style={{ width: "70px" }}>Status</th>
+                            <th style={{ width: "70px" }}>Usage</th>
                             
                           </tr>
                         </thead>
@@ -328,6 +329,7 @@ const BetaList = () => {
                               
                                 <td>{user.email}</td>
                                 <td>{user.status}</td>
+                                <td>{user.usage}</td>
                              
                             </tr>
                           ))}
