f77c38d94464c52c67b5580ade0b58dc007b7d77
OmriNach <omnachmani@gmail.com>
2023-10-15 15:20:40 -0400

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,11 +13,9 @@ const Sidebar = () => {
       <div className="sidebar">
         {/* Sidebar user panel (optional) */}
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="image">
-            <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
-          </div>
+   
           <div className="info">
-            <a href="#" className="d-block">Omri Nach</a>
+            <a href="#" className="d-block">Admin Account</a>
           </div>
 
         </div>
