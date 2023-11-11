06d21dd6d5e6ef183d82d51dc5b898d0785a3d5e
OmriNach <omnachmani@gmail.com>
2023-11-11 09:37:48 -0500

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
       {/* Brand Logo */}
-      <a href="index3.html" className="brand-link">
+      <a href="/dashboard" className="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
         <span className="brand-text font-weight-light">Admin Panel</span>
       </a>
@@ -15,7 +15,7 @@ const Sidebar = () => {
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
    
           <div className="info">
-            <a href="#" className="d-block">Admin Account</a>
+            <a href="/dashboard" className="d-block">Admin Account</a>
           </div>
 
         </div>
